339561bb90a82acc9dbd339df45201409640edf6
Egor Villanueva <eviespi@gmail.com>
2023-04-07 21:24:38 -0500

    Comenzando seccion 7

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent {
+
+  changeTheme(theme: string) {
+    
+    const linkTheme = document.querySelector('#theme');
+    const url = `./assets/css/colors/${ theme }.css`
+    linkTheme?.setAttribute('href', url);
+    
+    
+  }
+
+}
